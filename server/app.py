37688b94596eab7de9e3291855cefacb8ea83f2a
+ <+>
2024-07-02 16:16:24 +0300

    changes

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
-# server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -14,14 +14,37 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
+@app.route('/earthquakes/<int:earthquake_id>', methods=['GET'])
+def get_earthquake(earthquake_id):
+    earthquake = Earthquake.query.get(earthquake_id)
+    if earthquake:
+        return jsonify(earthquake.to_dict())
+    else:
+        return jsonify({'message': f'Earthquake {earthquake_id} not found.'}), 404
 
-# Add views here
+@app.route('/earthquakes', methods=['GET'])
+def get_earthquakes():
+    earthquakes = Earthquake.query.all()
+    return jsonify([earthquake.to_dict() for earthquake in earthquakes])
 
+@app.route('/earthquakes', methods=['POST'])
+def create_earthquake():
+    data = request.get_json()
+    earthquake = Earthquake(
+        magnitude=data['magnitude'],
+        depth=data['depth'],
+        latitude=data['latitude'],
+        longitude=data['longitude'],
+        location=data['location'],
+        time=datetime.fromisoformat(data['time'])
+    )
+    db.session.add(earthquake)
+    db.session.commit()
+    return jsonify(earthquake.to_dict()), 201
 
-if __name__ == '__main__':
+if __name__ == 'main':
     app.run(port=5555, debug=True)
